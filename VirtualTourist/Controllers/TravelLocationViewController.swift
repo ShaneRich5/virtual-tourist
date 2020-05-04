fcba5e88b8d840b73b1494717df3bfed6fabba57
Shane Richards <shane.richards@fticonsulting.com>
2020-05-03 23:14:32 -0400

    created the initial view controller and deleted the boilerplate; #1

---
+++ VirtualTourist/Controllers/TravelLocationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TravelLocationViewController.swift
+//  VirtualTourist
+//
+//  Created by Shane Richards on 5/3/20.
+//  Copyright © 2020 Shane Richards. All rights reserved.
+//
+
+import UIKit
+
+class TravelLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
