9f243c473f71965b909b31a724937cf3d2edde75
Shane Richards <shane.richards@fticonsulting.com>
2020-05-04 00:39:45 -0400

    added logic to add pins on touch; #1

--- VirtualTourist/Controllers/TravelLocationViewController.swift
+++ VirtualTourist/Controllers/TravelLocationViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import MapKit
 
 class TravelLocationViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureMap()
     }
+    
+    func configureMap() {
+        mapView.delegate = self
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(mapTapped))
+        mapView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func mapTapped(sender: UIGestureRecognizer) {
+        print("map tapped, \(sender.state)")
+        
+        if sender.state == .recognized {
+            print("state recognized")
+            let locationInView = sender.location(in: mapView)
+            let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
+            addAnnotation(location: locationOnMap)
+        } else if sender.state == .began {
+            print("state recognized")
+        } else if sender.state == .changed {
+           print("state changed")
+       }
+    }
+    
+    func addAnnotation(location: CLLocationCoordinate2D){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Some Title"
+        annotation.subtitle = "Some Subtitle"
+        self.mapView.addAnnotation(annotation)
+    }
+}
+
+extension TravelLocationViewController: MKMapViewDelegate {
+    
 }
