9d38075f6b764fc894cb9e4a4e110cf6999da2f6
Shane Richards <shane.richards@fticonsulting.com>
2020-05-16 22:41:29 -0400

    feat: struct to capture url; #5

---
+++ VirtualTourist/Network/FlickrClient.swift
@@ -0,0 +1,23 @@
+//
+//  FlickrClient.swift
+//  VirtualTourist
+//
+//  Created by Shane Richards on 5/16/20.
+//  Copyright © 2020 Shane Richards. All rights reserved.
+//
+
+import Foundation
+
+
+class FlickrClient {
+    struct Api {
+        let scheme = "https"
+        let host = "api.flickr.com"
+        let path = "/services/rest"
+        
+        func buildUrl() -> URL {
+            var url = "\(scheme)://\(host)\(path)"
+            return URL(string: url)!
+        }
+    }
+}
